bd8a30596b57805d0d56fa50f297541e49826098
Jordan Kline <jordan.kline@gmail.com>
2018-01-19 00:17:27 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,6 +11,7 @@ class Scraper
       student_hash = {}
       student_hash[:name] = el.css(".student-name").text
       student_hash[:location] = el.css(".student-location").text
+      binding.pry
       student_hash[:profile_url] = el.css("a")[0]["href"]
       return_array << student_hash
     end
